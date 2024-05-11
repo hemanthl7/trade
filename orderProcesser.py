d52caf00f57d86a85c17d83213c7f14c8774cefd
hemanthl7 <93383813+hemanthl7@users.noreply.github.com>
2024-05-11 20:04:46 +0100

    t (#2)
    * auto trade
    
    * Update .gitignore
    
    * Update users.csv
    
    * trade (#1)
    
    * auto trade
    
    * Update .gitignore
    
    * Update users.csv

---
+++ orderProcesser.py
@@ -0,0 +1,67 @@
+from SmartApi import SmartConnect
+import pyotp
+import pandas as pd
+import concurrent.futures
+import SmartApi.smartExceptions as se
+
+
+class OrderProcessor:
+
+    def __init__(self, users_path):
+        df = pd.read_csv(users_path)
+        self.users = {}
+        self.user_sessions = {}
+        for index, row in df.iterrows():
+            user_data = row.to_dict()
+            user_connect = SmartConnect(api_key=user_data['apiKey'])
+            user_session = user_connect.generateSession(user_data['userid'], user_data['pin'],
+                                                        pyotp.TOTP(user_data['totpKey']).now())
+            self.user_sessions[user_data['userid']] = user_session
+            self.users[user_data['userid']] = user_connect
+
+    def process(self, csv):
+        df = pd.read_csv(csv)
+        orders = list()
+        for index, row in df.iterrows():
+            order = row.to_dict()
+            orders.append(order)
+        self.run_orders(orders)
+
+    def run_orders(self, orders):
+        futures = []
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for order in orders:
+                futures.append(executor.submit(self.execute_orders, order_params=order))
+            for future in concurrent.futures.as_completed(futures):
+                print(future.result())
+
+    def execute_orders(self, order_params):
+        futures = []
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for user_id, user_connect, in self.users.items():
+                futures.append(executor.submit(self.execute_user_order, user_id=user_id, user_connect=user_connect,
+                                               order_params=order_params))
+            for future in concurrent.futures.as_completed(futures):
+                print(future.result())
+
+    def execute_user_order(self, user_id, user_connect, order_params):
+        try:
+            orderId = user_connect.placeOrder(order_params)
+            print("The order id is: {}".format(orderId))
+        except se.TokenException as e:
+            tokenSet = user_connect.renewAccessToken()
+            user_connect.setRefreshToken(tokenSet['refreshToken'])
+            user_connect.setAccessToken(tokenSet['jwtToken'])
+            print("{} User Session issue: {}".format(user_id, e.message))
+            self.execute_user_order(user_id, user_connect, order_params)
+
+        except Exception as e:
+            print("Order placement failed: {}".format(e.message))
+
+    def stop_sessions(self):
+        futures = []
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for user_id, user_connect, in self.users.items():
+                futures.append(executor.submit(user_connect.terminateSession, clientCode=user_id))
+            for future in concurrent.futures.as_completed(futures):
+                print(future.result())
---
+++ run.py
@@ -0,0 +1,36 @@
+import watchdog.events
+import watchdog.observers
+import time
+from orderProcesser import OrderProcessor
+
+
+class Handler(watchdog.events.PatternMatchingEventHandler):
+    def __init__(self, order_processor):
+        # Set the patterns for PatternMatchingEventHandler
+        watchdog.events.PatternMatchingEventHandler.__init__(self, patterns=['*.csv'],
+                                                             ignore_directories=True, case_sensitive=False)
+        self.order_processor = order_processor
+
+    def on_created(self, event):
+        print("Watchdog received created event - % s." % event.src_path)
+        # Event is created, you can process it now
+        self.order_processor.process(event.src_path)
+
+
+if __name__ == "__main__":
+    src_path = r"./orders/toplace"
+    des_path = r"./orders/done"
+    users_path = r"./uselogin/users.csv"
+    order_processor = OrderProcessor(users_path)
+    event_handler = Handler(order_processor)
+    observer = watchdog.observers.Observer()
+    observer.schedule(event_handler, path=src_path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except BaseException as e:
+        print(e)
+        order_processor.stop_sessions()
+        observer.stop()
+    observer.join()
