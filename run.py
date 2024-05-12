24bf08550af061e57c43f9434572bb31c0c5d1de
hemanthl7 <hemanth.lankipalli@gmail.com>
2024-05-12 14:37:33 +0100

    move file after process

--- run.py
+++ run.py
@@ -2,6 +2,11 @@ import watchdog.events
 import watchdog.observers
 import time
 from orderProcesser import OrderProcessor
+import shutil
+
+src_path = r"./orders/toplace"
+des_path = r"./orders/done"
+users_path = r"./uselogin/users.csv"
 
 
 class Handler(watchdog.events.PatternMatchingEventHandler):
@@ -15,12 +20,10 @@ class Handler(watchdog.events.PatternMatchingEventHandler):
         print("Watchdog received created event - % s." % event.src_path)
         # Event is created, you can process it now
         self.order_processor.process(event.src_path)
+        shutil.move(event.src_path, des_path)
 
 
 if __name__ == "__main__":
-    src_path = r"./orders/toplace"
-    des_path = r"./orders/done"
-    users_path = r"./uselogin/users.csv"
     order_processor = OrderProcessor(users_path)
     event_handler = Handler(order_processor)
     observer = watchdog.observers.Observer()
